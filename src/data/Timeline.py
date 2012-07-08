a65b4698422d7377dd7a2cd2d2724026e2d8f1a1
BetaS <k09089@naver.com>
2012-07-08 13:30:50 +0900

    timeline add

--- src/data/Timeline.py
+++ src/data/Timeline.py
@@ -1,6 +1,19 @@
 #coding: utf8
 
+from mypuppy.src.importer import ENTITY_DB
+
 class Timeline:
     @classmethod
-    def write_timeline(cls):
+    def write_timeline(cls, user_id, content, lat, lng, img_id=0, visiblity=0):
+        meta = cls.build_meta(visiblity)
+        ENTITY_DB.insert({"user_id": user_id, "content": content, "lat": lat, "lng": lng, "img_id": img_id, "meta": meta}, tb="timeline")
+
+
+    @classmethod
+    def delete_timeline(cls, content_id):
         pass
+
+    @classmethod
+    def build_meta(cls, visiblity):
+        result = {"visiblity": visiblity}
+        return str(result)
